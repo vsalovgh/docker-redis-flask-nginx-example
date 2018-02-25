0692d543d396aade4be2189d527e21a23535a844
Victor <vsalov@yahoo.com>
2018-02-25 03:14:44 -0800

    Timestamp changed to UTC

--- myapp/flask_app.py
+++ myapp/flask_app.py
@@ -1,17 +1,17 @@
 from flask import Flask, jsonify, request, render_template
 import time
+import datetime
 
 app = Flask(__name__)
 
-def ip(request):
-	return request.remote_addr
+def time_ip(request):
+	time_stamp = datetime.datetime.utcnow()
+	return time_stamp, request.remote_addr
 
 @app.route("/")
 @app.route("/v1/logs", methods=["GET", "POST"])
 def logs():
-	time_stamp = time.time()
-	address = ip(request)
-	print address
+	time_stamp, address = time_ip(request)
 	# the following is to be modified/sent to redis in near future
 	return jsonify({"logset": {"endpoint": "hello-world", "logs": [{str(time_stamp): str(address)}]}})
 
@@ -19,12 +19,10 @@ def logs():
 @app.route("/v1/helloworld/<name>", methods=['GET', 'POST'])
 def helloworld(name=None):
 	if name == None:
-		address = ip(request)
-		print address
+		address = time_ip(request)
 		return jsonify({"message": "hello world"})
 	elif name == 'logs':
-		address = ip(request)
-		time_stamp = time.time()
+		time_stamp, address = time_ip(request)
 		return jsonify({"logs": [{str(time_stamp): str(address)}]})
 
 if __name__ == "__main__":
