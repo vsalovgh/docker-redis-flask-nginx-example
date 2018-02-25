914e7381197899e2cd10937007a303764837e123
Victor <vsalov@yahoo.com>
2018-02-25 03:02:49 -0800

    Changed endpoint return values to match specs

--- myapp/flask_app.py
+++ myapp/flask_app.py
@@ -9,9 +9,11 @@ def ip(request):
 @app.route("/")
 @app.route("/v1/logs", methods=["GET", "POST"])
 def logs():
-	address = ip(request)
 	time_stamp = time.time()
-	return jsonify({"logs": [{str(time_stamp): str(address)}]})
+	address = ip(request)
+	print address
+	# the following is to be modified/sent to redis in near future
+	return jsonify({"logset": {"endpoint": "hello-world", "logs": [{str(time_stamp): str(address)}]}})
 
 @app.route("/v1/helloworld", methods=['GET', 'POST'])
 @app.route("/v1/helloworld/<name>", methods=['GET', 'POST'])
@@ -21,11 +23,9 @@ def helloworld(name=None):
 		print address
 		return jsonify({"message": "hello world"})
 	elif name == 'logs':
-		time_stamp = time.time()
 		address = ip(request)
-		print address
-		# the following is to be modified/sent to redis in near future
-		return jsonify({"logset": {"endpoint": "hello-world", "logs": [{str(time_stamp): str(address)}]}})
+		time_stamp = time.time()
+		return jsonify({"logs": [{str(time_stamp): str(address)}]})
 
 if __name__ == "__main__":
 	app.run()
